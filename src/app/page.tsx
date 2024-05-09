fae092fcb39e80f3f25ef8100711b4ffef2b5739
Jeriah Marson <jeriahmarson@gmail.com>
2024-05-09 14:23:47 -0700

    icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         </Navbar.Collapse>
       </Navbar>
 
-      <div className="bg-gradient-to-b from-blue-200 from-10% via-blue-100 via-70% to-white to-100%">
+      <div className="bg-gradient-to-b from-purple-400 from-10% via-purple-300 via-70% to-white to-100%">
         <div className="grid grid-cols-6 mx-7 py-8 ">
           <div className=" col-span-6 md:col-span-3 lg:col-span-2  md:w-full w-48 ml-20 md:ml-0">
             <div className="flex justify-center">
@@ -51,22 +51,34 @@ export default function Home() {
 
           <div className=" col-span-6 md:col-span-3 lg:col-span-4 lg:px-16 flex flex-col justify-between">
             <div className="">
-            <div className="flex flex-col items-center text-3xl pt-14">
-        <p className="text-3xl  font-bold">Software Developer</p>
-        <p className="text-2xl"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea hic illum autem, non aspernatur quia eum enim debitis sunt, dolore eius esse ipsam ut cumque dignissimos repellat. Aliquid, tenetur eligendi.</p>
-      </div>
-      <br />
+              <div className="flex flex-col items-center text-3xl pt-14">
+                <p className="text-3xl  font-bold">Software Developer</p>
+                <p className="text-2xl"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea hic illum autem, non aspernatur quia eum enim debitis sunt, dolore eius esse ipsam ut cumque dignissimos repellat. Aliquid, tenetur eligendi.</p>
+              </div>
+              <br />
               <div className=" grid grid-cols-2 pt-8">
                 <div className="text-2xl">
                   Languages:
                   <div>
                     <ul className="grid grid-cols-2">
-                      <li>C#</li>
-                      <li>HTML</li>
-                      <li>JS</li>
-                      <li>CSS</li>
-                      <li>TS</li>
-                      <li>SQL</li>
+                      <li>
+                        <i className="devicon-csharp-plain-wordmark"></i>
+                      </li>
+                      <li>
+                        <i className="devicon-html5-plain-wordmark colored"></i>
+                      </li>
+                      <li>
+                        <i className="devicon-javascript-plain colored"></i>
+                      </li>
+                      <li>
+                        <i className="devicon-css3-plain-wordmark colored"></i>
+                      </li>
+                      <li>
+                        <i className="devicon-typescript-plain colored"></i>
+                      </li>
+                      <li>
+                        <i className="devicon-azuresqldatabase-plain colored"></i>
+                      </li>
                       <li>TSX</li>
                     </ul>
                   </div>
@@ -74,11 +86,21 @@ export default function Home() {
                 <div className="text-2xl">
                   Frameworks:
                   <ul className="grid grid-cols-2">
-                    <li>Dotnet 7</li>
-                    <li>React</li>
-                    <li>Next JS</li>
-                    <li>Tailwind</li>
-                    <li>Bootstrap</li>
+                    <li>
+                      <i className="devicon-dot-net-plain-wordmark colored"></i>
+                    </li>
+                    <li>
+                      <i className="devicon-react-original-wordmark colored"></i>
+                    </li>
+                    <li>
+                      <i className="devicon-nextjs-original-wordmark"></i>
+                    </li>
+                    <li>
+                      <i className="devicon-tailwindcss-plain-wordmark colored"></i>
+                    </li>
+                    <li>
+                      <i className="devicon-bootstrap-plain-wordmark colored"></i>
+                    </li>
 
                   </ul>
                 </div>
@@ -86,37 +108,37 @@ export default function Home() {
             </div>
           </div>
         </div>
-        
-        
+
+
         <hr />
         <div className="flex flex-col items-center text-4xl font-titillium font-bold pt-14">
-        <p>Projects</p>
-      </div>
-      <div className="grid grid-cols-6 mx-7 py-8 px-2 gap-5">
-        <div className=" col-span-6 md:col-span-3 lg:col-span-2  md:w-full w-48 ml-20 md:ml-0">
-          <h1 className="text-3xl font-bold text-center">Court Monitor</h1>
-          <br />
-          <div className="courtmonitor w-[100%] h-40 md:h-[45vh] lg:h-[35vh] rounded-3xl"></div>
-          <br />
-          <h3 className="text-2xl text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati natus aliquam nisi delectus, quae quam, consequatur dolores aut officiis neque deleniti itaque facilis temporibus labore tenetur quis eum. Ducimus, consequuntur?</h3>
+          <p>Projects</p>
         </div>
+        <div className="grid grid-cols-6 mx-7 py-8 px-2 gap-5">
+          <div className=" col-span-6 md:col-span-3 lg:col-span-2  md:w-full w-48 ml-20 md:ml-0">
+            <h1 className="text-3xl font-bold text-center">Court Monitor</h1>
+            <br />
+            <div className="courtmonitor w-[100%] h-40 md:h-[45vh] lg:h-[35vh] rounded-3xl"></div>
+            <br />
+            <h3 className="text-2xl text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati natus aliquam nisi delectus, quae quam, consequatur dolores aut officiis neque deleniti itaque facilis temporibus labore tenetur quis eum. Ducimus, consequuntur?</h3>
+          </div>
 
-        <div className=" col-span-6 md:col-span-3 lg:col-span-2  md:w-full w-48 ml-20 md:ml-0">
-          <h1 className="text-3xl font-bold text-center">Court Monitor</h1>
-          <br />
-          <div className="courtmonitor w-[100%] h-40 md:h-[45vh] lg:h-[35vh] rounded-3xl"></div>
-          <br />
-          <h3 className="text-2xl text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati natus aliquam nisi delectus, quae quam, consequatur dolores aut officiis neque deleniti itaque facilis temporibus labore tenetur quis eum. Ducimus, consequuntur?</h3>
-        </div>
+          <div className=" col-span-6 md:col-span-3 lg:col-span-2  md:w-full w-48 ml-20 md:ml-0">
+            <h1 className="text-3xl font-bold text-center">Court Monitor</h1>
+            <br />
+            <div className="courtmonitor w-[100%] h-40 md:h-[45vh] lg:h-[35vh] rounded-3xl"></div>
+            <br />
+            <h3 className="text-2xl text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati natus aliquam nisi delectus, quae quam, consequatur dolores aut officiis neque deleniti itaque facilis temporibus labore tenetur quis eum. Ducimus, consequuntur?</h3>
+          </div>
 
-        <div className=" col-span-6 md:col-span-3 lg:col-span-2  md:w-full w-48 ml-20 md:ml-0">
-          <h1 className="text-3xl font-bold text-center">Court Monitor</h1>
-          <br />
-          <div className="courtmonitor w-[100%] h-40 md:h-[45vh] lg:h-[35vh] rounded-3xl"></div>
-          <br />
-          <h3 className="text-2xl text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati natus aliquam nisi delectus, quae quam, consequatur dolores aut officiis neque deleniti itaque facilis temporibus labore tenetur quis eum. Ducimus, consequuntur?</h3>
+          <div className=" col-span-6 md:col-span-3 lg:col-span-2  md:w-full w-48 ml-20 md:ml-0">
+            <h1 className="text-3xl font-bold text-center">Court Monitor</h1>
+            <br />
+            <div className="courtmonitor w-[100%] h-40 md:h-[45vh] lg:h-[35vh] rounded-3xl"></div>
+            <br />
+            <h3 className="text-2xl text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati natus aliquam nisi delectus, quae quam, consequatur dolores aut officiis neque deleniti itaque facilis temporibus labore tenetur quis eum. Ducimus, consequuntur?</h3>
+          </div>
         </div>
-      </div>
 
       </div>
 
@@ -124,7 +146,7 @@ export default function Home() {
         <div className="w-full">
 
           <div className="w-full sm:flex sm:items-center sm:justify-between">
-           
+
             <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
               <Footer.Icon href="#" icon={BsFacebook} />
               <Footer.Icon href="#" icon={BsInstagram} />
